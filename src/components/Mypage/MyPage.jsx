e62ae5bf9355b1875ffa7c761c362649ec6494fb
BY1502 <sks14502@gmail.com>
2024-09-13 11:09:53 +0900

    데이터

--- src/components/Mypage/MyPage.jsx
+++ src/components/Mypage/MyPage.jsx
@@ -29,10 +29,10 @@ function Mypage() {
       })
       .catch((err) => {
         console.error('Error fetching user info:', err);
-        if (err.response && err.response.status === 401) {
-          alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
-          window.location.href = '/login';
-        }
+        // if (err.response && err.response.status === 401) {
+        //   alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
+        //   window.location.href = '/login';
+        // }
       });
   }, []);
 
